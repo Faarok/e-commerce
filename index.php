0d619ec8fa158198e43819f04d1c482362faf4a4
Svein Samson <samson.svein@gmail.com>
2023-03-21 15:08:48 +0100

    Creation of logo and favicon. Chose the name of the project

--- index.php
+++ index.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="/img/favicon.png">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="stylesheet" href="css/style.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Les Théorie des Cordes</title>
 </head>
 <body>
     
