c189a76dbf046959b07bf4c5d149e30217e06e71
Svein Samson <samson.svein@gmail.com>
2023-03-21 12:49:50 +0100

    Initial commit

--- git-clone.sh
+++
@@ -1,6 +0,0 @@
-echo "Enter repo to clone (user/repo_name) : "
-read repo
-git clone https://github.com/${repo} .
-echo "Enter your new repo (user/repo_name) : "
-read new_repo
-git remote add origin https://github.come/${new_repo}
